d2e29b53e5a09081b92a2fd31aa315df74e16eb3
amaar <amaar.ahmednur@student.moringaschool.com>
2023-10-29 15:27:34 +0300

    setting changes in app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,34 @@ function App() {
   const [filteredTransactions, setFilteredTransaction] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
-  
+  useEffect(() => {
+    fetch('http://localhost:8001/transactions')
+      .then((response) => response.json())
+      .then((data) => {
+        setTransactions(data.transactions);
+      })
+      .catch((error) => console.error('Error fetching data: ', error));
+  }, []);
+
+  // Updated and filteredTransactions based on the search term
+  useEffect(() => {
+    const filtered = transactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTransactions(filtered);
+  }, [searchTerm, transactions]);
+
+  return (
+    <div className="App">
+      <h1>Bank Transactions</h1>
+      <TransactionForm
+        transactions={transactions}
+        setTransactions={setTransactions}
+      />
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <TransactionTable transactions={filteredTransactions} />
+    </div>
+  );
+}
 
 export default App;
